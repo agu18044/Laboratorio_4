620260144e278fcc40165e793fb9aaffe85872e9
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-08-16 18:50:00 -0600

    cambios

--- lab_4.X/main.s
+++ lab_4.X/main.s
@@ -1,3 +1,100 @@
+; Archivo:     main.S
+; Dispositivo: PIC16F887
+; Autor:       Diego Aguilar
+; Compilador:  pic-as (v2.30), MBPLABX v5.40
+;
+; Programa:    contador 
+; Hardware:    LEDs 
+;
+; Creado: 9 agosto, 2021
+; Última modificación: 9 agosto, 2021
 
+PROCESSOR 16F887
+ #include <xc.inc>
+ 
+;configuration word 1
+    CONFIG FOSC=INTRC_NOCLKOUT  // Oscilador interno sin salidas
+    CONFIG WDTE=OFF  // WDT disabled (reinicio repetitivo del pic)
+    CONFIG PWRTE=ON  // PWRT enabled (espera de 72ms al iniciar)
+    CONFIG MCLRE=OFF // El pin MCLR se utiliza como I/0
+    CONFIG CP=OFF    // Sin proteccion de codigo
+    CONFIG CPD=OFF   // Sin proteccion de datos
+    
+    CONFIG BOREN=OFF // Sin reinicio cuando el voltaje de alimentacion baja de 4V
+    CONFIG IESO=OFF  // Reinicio sin cambio de reloj de interno a externo
+    CONFIG FCMEN=OFF // Cambio de reloj externo a interno en caso de fallo
+    CONFIG LVP=OFF    // Programacion en bajo voltaje permitida
+    
+;configuration word 2
+    CONFIG WRT=OFF   // Proteccion de autoescritura por el programa desactivada
+    CONFIG BOR4V=BOR40V  // Reinicio abajo de 4V, (BOR21V=2.1V)
+    
+PSECT udata_bank0  ;common memory
+    reg:    DS 2
+    
+PSECT resVect, class=CODE, abs, delta=2
+;-----------vector reset--------------;
+ORG 00h     ;posicion 0000h para el reset
+resetVec:
+    PAGESEL main
+    goto main
+ 
+PSECT code, delta=2, abs
+ORG 100h    ; posicion para le codigo
+ tabla:
+    clrf    PCLATH
+    bsf	    PCLATH, 0   ;PCLATH = 01
+    andlw   0x0f
+    addwf   PCL         ;PC = PCLATH + PCL
+    ; se configura la tabla para el siete segmentos
+    retlw   00111111B  ;0
+    retlw   00000110B  ;1
+    retlw   01011011B  ;2
+    retlw   01001111B  ;3
+    retlw   01100110B  ;4
+    retlw   01101101B  ;5
+    retlw   01111101B  ;6
+    retlw   00000111B  ;7
+    retlw   01111111B  ;8
+    retlw   01100111B  ;9
+    retlw   01110111B  ;A
+    retlw   01111100B  ;B
+    retlw   00111001B  ;C
+    retlw   01011110B  ;D
+    retlw   01111001B  ;E
+    retlw   01110001B  ;F
+    
+ ;-----------configuracion--------------;
 
+main:
+    call    config_io
+    banksel PORTA
 
+loop:
+  
+ 
+    goto    loop
+
+config_io:
+    ; Configuracion de los puertos
+    banksel ANSEL	; Se selecciona bank 3
+    clrf    ANSEL	; Definir puertos digitales
+    clrf    ANSELH
+    
+    banksel TRISA	; Banco 01
+    clrf    TRISA	;salida contador 1
+    clrf    TRISC	;salida display 1
+    bsf	    TRISB, 0
+    bsf	    TRISB, 1
+    clrf    TRISD	;salida display 2 
+    clrf    TRISE	;salida contador 2
+    
+    banksel PORTA	; Banco 00
+    clrf    PORTB
+    clrf    PORTA
+    clrf    PORTC
+    clrf    PORTD
+  
+    return
+
+end
